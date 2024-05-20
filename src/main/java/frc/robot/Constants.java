49f3ace3ad17ebf43b019e30b186db72163f909e
NeatTeam Software <neatteamsoftware@gmail.com>
2024-05-20 21:59:45 +0300

    stoller shapira adam

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,9 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static class AsafConstants {
+    public static final int kShragaPort = 1;
+  }
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
